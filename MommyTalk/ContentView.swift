4ff8065bcd52856d36b7586a1045dfc0197179bc
Sanghwi Back <sanghwiback@gmail.com>
2020-08-18 08:20:29 +0900

    Initial Commit

---
+++ MommyTalk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MommyTalk
+//
+//  Created by 백상휘 on 2020/08/18.
+//  Copyright © 2020 Sanghwi Back. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
